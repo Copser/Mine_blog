b7e677b1be00a2b7c1d7fbb506d8df7f4d572522
Copser <iamrmot@gmail.com>
2014-11-08 07:02:04 +0100

    Upgraded to Django 1.7

--- django_tutorial_blog_ng/settings.py
+++ django_tutorial_blog_ng/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     'blogengine',
     'django.contrib.sites',
     'django.contrib.flatpages',
